d0fe5c33b503a13cd13faef5fd355c037742b6f4
Sarthak <sarthaksingh5710@gmail.com>
2024-03-20 19:00:50 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import Login from './Components/Login'
+import Home from './Components/Home'
+import ProtectedRoute from './Components/ProtectedRoute'
+import NotFound from './Components/NotFound'
+import Jobs from './Components/Jobs'
+import JobItemDetails from './Components/JobItemDetails'
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
---
+++ src/Components/Header/index.js
@@ -0,0 +1,42 @@
+import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const Header = props => {
+  const onLogoutClick = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="navbar-container">
+      <div>
+        <Link to="/" className="link-item">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="website-logo"
+          />
+        </Link>
+      </div>
+      <ul className="header-list-items">
+        <Link to="/" className="link-item">
+          <li className="home-heading home">Home</li>
+        </Link>
+        <Link to="/jobs" className="link-item">
+          <li className="job-heading home">Jobs</li>
+        </Link>
+        <li>.</li>
+      </ul>
+      <div>
+        <button type="button" className="logout-button" onClick={onLogoutClick}>
+          Logout
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
